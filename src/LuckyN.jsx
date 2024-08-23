d993cdfce9abc275eb3a6a6b28c36d290a3c92b9
Aswathy k.r <aswathy19kr@gmail.com>
2024-08-23 20:33:05 +0530

    added a button to re-roll

--- src/LuckyN.jsx
+++ src/LuckyN.jsx
@@ -4,12 +4,13 @@ import {getRolls,sum} from './ulits'
 function LuckyN({numDice=3, goal=9}){
     const [dice,setDice] = useState(getRolls(numDice))
     const isWinner = sum(dice) === goal
+    const roll = () => setDice(getRolls(numDice))
     console.log(dice);
     return(
         <main>
             <h1>Lucky{goal}  <span style={{color:"blue"}}>{isWinner && "You Win!!"}</span></h1>
             <Dice dice={dice}/>
-           
+            <button onClick={roll}>Re-Roll Dice</button>
         </main>
     )
 }
